dc7df1ffb3fa08119b78e7d5800303b08d344670
HydroponKing <momomolchenel@gmail.com>
2025-03-08 19:21:22 +0500

    fix error with SVG

--- src/assets/assets.tsx
+++ src/assets/assets.tsx
@@ -1,6 +1,6 @@
-import React, {SVGProps} from "react";
+import {SVGProps} from "react";
 
-export const AlarmSvg  = (props: SVGProps<SVGElement>) =>
+export const AlarmSvg  = (props: SVGProps<SVGSVGElement>) =>
     <svg
         {...props}
         viewBox="0 0 24.00 24.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"
